0d144ddacdb2767f95ddee5d91ea1adaf29d41d0
Juan David H <juan.hernandez@anomali.com>
2020-10-10 17:44:31 -0500

    Added return statement

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
