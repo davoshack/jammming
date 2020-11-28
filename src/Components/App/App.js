d7a5bcea81c5f4fa721954091b850bc9c3230d3e
Juan David H <juan.hernandez@anomali.com>
2020-11-28 18:36:51 -0500

    Implemented Spotify search request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -48,8 +48,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term)
-
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
   }
 
   render() {
